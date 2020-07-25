2eff3f7542c0224fac831529537d730814b54fd9
Jeesup <mrpatiworld@gmail.com>
2020-07-26 00:48:40 +0200

    Utworzenie projektu, uzupelnienie go podstawowymi elementami, dodanie kontroli nad obiektem gracza, utworzenie planszy testowej.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject playerObject;
+    [SerializeField] Vector3 offset;
+    void Start()
+    {
+        offset = playerObject.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerObject.transform.position - offset;
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+
+    float movementSpeed = 200f;
+    float jumpForce = 500f;
+
+
+    enum PlayerMovementState{
+        Landed,
+        MidAir
+        
+    }
+
+    [SerializeField] PlayerMovementState playerMovementState = PlayerMovementState.Landed;
+   void Start()
+    {     
+        rigidbody = GetComponent<Rigidbody>();
+       
+    }
+    void FixedUpdate()
+    {
+
+        CheckInput();
+    }
+
+
+    void CheckInput(){
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            rigidbody.AddForce(new Vector3(movementSpeed * Time.deltaTime,0f,0f));
+        }
+        if(Input.GetKey(KeyCode.D)){
+            rigidbody.AddForce(new Vector3(-movementSpeed* Time.deltaTime,0f,0f));
+        }
+
+
+    }
+    void Jump()
+    {
+          if(Input.GetKey(KeyCode.Space) && playerMovementState == PlayerMovementState.Landed){
+
+            rigidbody.AddForce(new Vector3(0f,jumpForce,0f));
+            playerMovementState = PlayerMovementState.MidAir;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.transform.tag.Equals("Friendly")){
+            playerMovementState = PlayerMovementState.Landed;
+        }    
+    }
+}
